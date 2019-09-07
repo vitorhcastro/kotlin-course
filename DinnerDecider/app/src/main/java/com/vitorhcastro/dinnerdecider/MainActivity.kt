3207edef026a54d419032ba8d267ea765b3674cd
Vitor Castro <vitorhugocastro95@gmail.com>
2019-09-07 01:28:38 +0100

    DinnerDecider Part 3

--- DinnerDecider/app/src/main/java/com/vitorhcastro/dinnerdecider/MainActivity.kt
+++ DinnerDecider/app/src/main/java/com/vitorhcastro/dinnerdecider/MainActivity.kt
@@ -3,13 +3,26 @@ package com.vitorhcastro.dinnerdecider
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    var foodList = arrayListOf("Chinese", "Hamburguer", "Pizza", "McDonald's", "Barros Pizza")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        selectedFoodTxt.text = "Burger King"
+        decideBtn.setOnClickListener{
+            val random = Random()
+            var randomFood = random.nextInt(foodList.count())
+            selectedFoodTxt.text = foodList[randomFood]
+        }
+
+        addFoodBtn.setOnClickListener {
+            val newFood = addFoodTxt.text.toString()
+            foodList.add(newFood)
+            addFoodTxt.text.clear()
+        }
     }
 }
