ebade759a83c240eade3da456fc44c3a74453f94
Vitor Castro <vitorhugocastro95@gmail.com>
2019-09-07 22:31:28 +0100

    Fixed some bugs

--- Exercises/MakeYourOwnDecider/app/src/main/java/com/vitorhcastro/makeyourowndecider/MainActivity.kt
+++ Exercises/MakeYourOwnDecider/app/src/main/java/com/vitorhcastro/makeyourowndecider/MainActivity.kt
@@ -1,35 +1,37 @@
 package com.vitorhcastro.makeyourowndecider
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import androidx.core.view.isVisible
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    val foodArray = arrayListOf<String>()
+    private val foodArray = arrayListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        addFoodBtn.setOnClickListener {
-            val newFood = addFoodTxt.text.toString()
-            if(!foodArray.contains((newFood))){
+        addOptionBtn.setOnClickListener {
+            val newFood = addOptionTxt.text.toString()
+
+            if(!foodArray.contains((newFood)) && newFood.isNotBlank()){
                 foodArray.add(newFood)
+
                 if(foodArray.count() > 1){
                     decideBtn.visibility = View.VISIBLE
                 }
             }
-            addFoodTxt.text.clear()
+
+            addOptionTxt.text.clear()
         }
 
         decideBtn.setOnClickListener {
             val randomFoodNumber = Random().nextInt(foodArray.count())
             val chosenFood = foodArray[randomFoodNumber]
-            selectedFoodTxt.text = chosenFood
+            chosenOptionTxt.text = chosenFood
         }
     }
 }
