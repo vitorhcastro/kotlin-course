c1283c86a0cd22337b61f3787d47c95d8e5eed76
Vitor Castro <vitorhugocastro95@gmail.com>
2019-09-09 23:55:23 +0100

    Variables with Kotlin

---
+++ Lessons/Kotlin Foundation/src/Variables.kt
@@ -0,0 +1,24 @@
+fun main(args: Array<String>) {
+    var name = "JonnyB"
+
+    var isAwesome = true
+    isAwesome = false
+
+    println("Is " + name + " awesome? The answer is: " + isAwesome)
+
+    var a = 3
+    var b = 6
+    println(a + b)
+
+    val doubleNum: Double = 123.45 // 64bit number
+    val floatNum: Float = 123.45f // 32bit number
+
+    val longNum: Long = 1237819283712L // 64 bit
+
+    val aDouble = a.toString()
+
+    var hero: String
+    hero = "Batman"
+    hero = "Superman"
+    println(hero)
+}
