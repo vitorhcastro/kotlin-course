f6ee701d6b72336cdeafe693e82d3968baa4cccb
Vitor Castro <vitorhugocastro95@gmail.com>
2019-11-26 04:29:42 +0000

    [CoderSwag] Product Recycle Adapter

--- CoderSwag/app/src/main/java/com/vhcastro/coderswag/controller/ProductsActivity.kt
+++ CoderSwag/app/src/main/java/com/vhcastro/coderswag/controller/ProductsActivity.kt
@@ -2,16 +2,26 @@ package com.vhcastro.coderswag.controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
 import com.vhcastro.coderswag.R
+import com.vhcastro.coderswag.adapters.ProductAdapter
+import com.vhcastro.coderswag.services.DataService
 import com.vhcastro.coderswag.utilities.EXTRA_CATEGORY
+import kotlinx.android.synthetic.main.activity_products.*
 
 class ProductsActivity : AppCompatActivity() {
+
+    lateinit var adapter: ProductAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_products)
-        
+
         val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
-        println(categoryType)
+        adapter = ProductAdapter(this, DataService.getProducts(categoryType))
+
+        val layoutManager = GridLayoutManager(this, 2)
+        productsListView.layoutManager = layoutManager
+        productsListView.adapter = adapter
     }
 }
