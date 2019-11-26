978559525c18b5dde1baa349d6fe89f77735fe11
Vitor Castro <vitorhugocastro95@gmail.com>
2019-11-26 03:20:44 +0000

    [CoderSwag] Custom List Adapter

--- CoderSwag/app/src/main/java/com/vhcastro/coderswag/controller/MainActivity.kt
+++ CoderSwag/app/src/main/java/com/vhcastro/coderswag/controller/MainActivity.kt
@@ -4,19 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import com.vhcastro.coderswag.R
+import com.vhcastro.coderswag.adapters.CategoryAdapter
 import com.vhcastro.coderswag.model.Category
 import com.vhcastro.coderswag.services.DataService
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : ArrayAdapter<Category>
+    lateinit var adapter : CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, DataService.categories)
+        adapter = CategoryAdapter(this, DataService.categories)
         categoryListView.adapter = adapter
     }
 }
--- CoderSwag/app/src/main/java/com/vhcastro/coderswag/services/DataService.kt
+++ CoderSwag/app/src/main/java/com/vhcastro/coderswag/services/DataService.kt
@@ -8,7 +8,7 @@ object DataService {
     val categories = listOf(
         Category("SHIRTS", "shirtimage"),
         Category("HOODIES", "hoodieimage"),
-        Category("HATS", "hatsimage"),
+        Category("HATS", "hatimage"),
         Category("DIGITAL", "digitalgoodsimage")
     )
 
