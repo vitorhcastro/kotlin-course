9099fde098bac1f3349633ec1c90e18781ef4456
Vitor Castro <vitorhugocastro95@gmail.com>
2019-11-26 08:26:36 +0000

    [Smack] CreateUserActivity UI

--- Smack/app/src/main/java/com/vhcastro/smack/RegistrationActivity.kt
+++ Smack/app/src/main/java/com/vhcastro/smack/RegistrationActivity.kt
@@ -1,19 +1,25 @@
 package com.vhcastro.smack
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 
-class LoginActivity : AppCompatActivity() {
+class RegistrationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_registration)
+    }
+
+    fun generateUserAvatar(view: View){
+
+    }
+
+    fun generateColorClicked(view: View){
+
     }
 
     fun createUserBtnClicked(view: View){
-        var registerUserIntent = Intent(this, RegistrationActivity::class.java)
-        startActivity(registerUserIntent)
+
     }
 }
