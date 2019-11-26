24ff67f834d5e790635cd61cae66b0bf7374bb29
Vitor Castro <vitorhugocastro95@gmail.com>
2019-11-26 03:05:32 +0000

    [CoderSwag] Simple ListView

--- CoderSwag/app/src/main/java/com/vhcastro/coderswag/model/Category.kt
+++ CoderSwag/app/src/main/java/com/vhcastro/coderswag/model/Category.kt
@@ -1,3 +1,7 @@
 package com.vhcastro.coderswag.model
 
-class Category(val title: String, val image: String)
+class Category(val title: String, val image: String){
+    override fun toString(): String {
+        return title
+    }
+}
